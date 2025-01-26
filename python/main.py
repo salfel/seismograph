a242102e741f2dbdec1b12b42351d5ca20f6772c
Felix Salcher <dev.felix.salcher@gmail.com>
2025-01-26 15:09:50 +0100

    fix serial display

--- python/main.py
+++ python/main.py
@@ -3,25 +3,28 @@ import matplotlib.animation as animation
 from serial import Serial
 import numpy as np
 
-# ser = Serial(
-#     port="/dev/ttyUSB0",
-#     baudrate=9600,
-#     timeout=1,
-# )
+serial = Serial(
+    port="/dev/ttyUSB0",
+    baudrate=9600,
+)
 
 # try:
 #     while True:
-#         if ser.in_waiting > 0:  # Check if data is available
-#             data = ser.readline()  # Read a line of data
-#             print(data.decode("utf-8").strip())  # Decode and print the data
-# except KeyboardInterrupt:
-#     print("Exiting...")
+#         if serial.in_waiting > 0:
+#             data = serial.readline().decode("utf-8").strip()
+#             print(data)
 # finally:
-#     ser.close()  # Close the serial port
+#     serial.close()
 
 
 class SerialWatcher:
-    def __init__(self):
+    count = 0
+
+    def __init__(self, serial: Serial):
+        self.serial = serial
+        while self.serial.in_waiting > 0:
+            self.serial.readline()
+
         fix, ax = plt.subplots()
         x_data, y_data = [], []
         (line,) = plt.plot(x_data, y_data)
@@ -32,41 +35,41 @@ class SerialWatcher:
         self.ax = ax
 
         ani = animation.FuncAnimation(
-            fix, self.update, frames=np.arange(1000), interval=50, blit=True
+            fix, self.update, frames=np.arange(10000), interval=50
         )
 
         plt.show()
 
     def update(self, frame):
-        new_x = frame * 0.1
-        new_y = np.sin(new_x)
+        if self.serial.in_waiting > 0:  # Check if data is available
+            data = self.serial.readline().decode("utf-8").strip()  # Read a line of data
+            print(self.serial.in_waiting)
 
-        # Append new data to the lists
-        self.x_data.append(new_x)
-        self.y_data.append(new_y)
+            new_x = self.count
+            new_y = int(data)
 
-        # Update the line data
-        self.line.set_data(self.x_data, self.y_data)
+            self.last_value = new_y
 
-        # Adjust the x-axis and y-axis limits to fit the new data
-        self.ax.relim()  # Recalculate limits
-        self.ax.autoscale_view()  # Rescale the view
+            # Append new data to the lists
+            self.x_data.append(new_x)
+            self.y_data.append(new_y)
+
+            self.x_data = self.x_data[-100:]
+            self.y_data = self.y_data[-100:]
+
+            # Update the line data
+            self.line.set_data(self.x_data, self.y_data)
+
+            # Adjust the x-axis and y-axis limits to fit the new data
+            self.ax.relim()  # Recalculate limits
+            self.ax.autoscale_view()  # Rescale the view
+
+            self.count += 1
 
         return (self.line,)
 
 
-# def animate(i):
-#     graph_data = open("example.txt", "r").read()
-#     lines = graph_data.split("\n")
-#     xs = []
-#     ys = []
-#     for line in lines:
-#         if len(line) > 1:
-#             x, y = line.split(",")
-#             xs.append(float(x))
-#             ys.append(float(y))
-#     ax1.clear()
-#     ax1.plot(xs, ys)
-#
-
-serial = SerialWatcher()
+try:
+    SerialWatcher(serial)
+finally:
+    serial.close()
